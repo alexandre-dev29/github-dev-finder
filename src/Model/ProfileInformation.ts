3f58052c2f71e5ec586c25520ee49bdf13881706
alexandre-dev29 <axel.business29@gmail.com>
2021-11-17 09:06:01 +0200

    Adding ProfileInformation.ts

---
+++ src/Model/ProfileInformation.ts
@@ -0,0 +1,17 @@
+import GithubImage from "../Images/githubImage.jpg";
+
+export class ProfileInformation {
+  imageUrl: string = GithubImage;
+  bio: string = "";
+  userName: string = "";
+  completeName: string = "";
+  repository: number = 0;
+  followers: number = 0;
+  following: number = 0;
+  organisation: string = "";
+  website: string = "";
+  twitterAccount: string = "";
+  dateJoined: string = "";
+  location: string = "";
+  url: string = "";
+}
